3d934abe6c07583f953805dbaf075f43cc27ff3f
Johnny Silva <johnnysilva.dev@gmail.com>
2024-05-18 12:13:55 -0300

    feat: adding projects

--- src/app/modules/portifolio/pages/home/home.component.ts
+++ src/app/modules/portifolio/pages/home/home.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { KnowledgeComponent } from '../../components/knowledge/knowledge.component';
 import { ExpiriencesComponent } from '../../components/expiriences/expiriences.component';
+import { ProjectsComponent } from '../../components/projects/projects.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, KnowledgeComponent, ExpiriencesComponent],
+  imports: [
+    HeaderComponent,
+    KnowledgeComponent,
+    ExpiriencesComponent,
+    ProjectsComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
